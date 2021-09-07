6a393e796c9897e46066ed0feed14538f93ab676
alexiaruizq <53828050+alexiaruizq@users.noreply.github.com>
2021-09-06 22:43:28 -0700

    Add files via upload

---
+++ PracticaConstraints/PracticaConstraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaConstraints
+//
+//  Created by Alexia Ruiz Quiroz on 06/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
